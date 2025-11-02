0e90620d3cdd625bad896be2d8a6785ae6b0146f
Pratheek Gowda <pratheek@myyahoo.com>
2025-11-02 14:41:02 +0530

    Update 404.js

--- admin-dashboard/pages/404.js
+++ admin-dashboard/pages/404.js
@@ -1,1 +1,9 @@
 
+export default function NotFound() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'Arial' }}>
+      <h1>404 - Page Not Found</h1>
+      <a href="/">Go to Login</a>
+    </div>
+  );
+}
