93936e50e2134233ee45bb977fa45b6c0ec1acf9
Pratheek Gowda <pratheek@myyahoo.com>
2025-11-02 14:22:37 +0530

    Delete pages directory

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>This page does not exist</p>
-      <a href="/">Go back to login</a>
-    </div>
-  );
-}
