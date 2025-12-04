86d832200e15af44cc69a9c3e292c4a2583b9099
Pratheek Gowda <pratheek@myyahoo.com>
2025-12-04 17:45:57 +0530

    Update get-user-referrals.js

--- netlify/functions/get-user-referrals.js
+++ netlify/functions/get-user-referrals.js
@@ -7,18 +7,15 @@ const pool = new Pool({
 
 exports.handler = async (event) => {
   try {
-    const userId = event.queryStringParameters?.userId;
-
     const result = await pool.query(`
       SELECT 
         r.id, r.referred_name, r.referred_email, r.referred_phone, r.order_details,
         r.status, r.created_at, r.approved_at, r.approved_by_admin,
-        rl.referral_code, rl.operator
+        rl.referral_code, rl.operator, rl.user_id
       FROM referrals r
       JOIN referral_links rl ON r.referral_link_id = rl.id
-      WHERE rl.user_id = $1
       ORDER BY r.created_at DESC
-    `, [userId]);
+    `);
 
     return {
       statusCode: 200,
